44156a422b5bbbb345a11f3ff3430650677daefb
sbooeshaghi <sbooeshaghi@gmail.com>
2021-11-01 12:38:32 -0700

    initial data dump

---
+++ analysis/293T/obs7/kbcount_cdna.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# FQ=../../../../data/293T/rep3/lane1/cdna/ && BASE=../../../../reference/293T/cdna && ./kbcount_cdna.sh -x 10xv3 -o cdna_out/ -i $BASE/index.idx -g $BASE/t2g.txt -f $FQ
+
+usage () {
+    echo "Usage: $0 [options]
+    
+    Options:
+    -o, --output            output folder
+    -i, --index             pseudoalignment index
+    -g, --genemap           transcripts to genes map
+    -x, --technology        single-cell tech
+    -f, --fastqdir          folder containing fastqs
+    "
+    exit 1
+}
+
+while getopts ":o:i:w:g:x:f:" opt; do
+    case $opt in
+        o|--output)
+            OUTDIR=$OPTARG
+            ;;
+        i|--index)
+            INDEX=$OPTARG
+            ;;
+        g|--genemap)
+            T2G=$OPTARG
+            ;;
+        x|--technology)
+            TECH=$OPTARG
+            ;;
+        f|--fastqdir)
+            FASTQDIR=$OPTARG
+            ;;
+        h)
+            usage
+            ;;
+        \?)
+            echo "Invalid argument"
+            usage
+            ;;
+        :)
+            echo "Add arguments"
+            usage
+            ;;
+    esac
+done
+
+# check options        
+if [ -z "$OUTDIR" -o -z "$INDEX" -o -z "$T2G" -o -z "$TECH" -o -z "$FASTQDIR" ]
+then
+    echo "Error"
+    usage
+fi
+
+
+# begin workflow
+kb count \
+--h5ad \
+--filter \
+-t 16 \
+-m 16G \
+-o $OUTDIR \
+-i $INDEX \
+-g $T2G \
+-x $TECH \
+$(paste -d" " \
+  <(ls $FASTQDIR | awk -v p=$FASTQDIR '{print p$0}' | grep R1) \
+  <(ls $FASTQDIR | awk -v p=$FASTQDIR '{print p$0}' | grep R2))
---
+++ analysis/293T/obs7/out/README.md
@@ -0,0 +1,1 @@
+cells_x_genes.mtx.gz was split in two so that it could be uploaded to GitHub
