82081995d2588fa5178bd4373dad5b0784f4dff6
sbooeshaghi <sbooeshaghi@gmail.com>
2021-11-03 14:06:14 -0700

    changed assignment to include barcode and entropy, cleaned up repo

---
+++ analysis/293T/obs6/assign.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+gunzip out/*.gz
+../../assign/mx_index.py assign/markers.txt assign/
+../../assign/mx_select.py assign/markers.txt out/genes.txt assign/
+../../assign/mx_extract.py out/matrix.mtx assign/select.txt assign/matrix_select.mtx
+../../assign/mx_assign.py assign/matrix_select.mtx assign/markers.ec assign/assignments.txt.gz out/barcodes.txt
+gzip out/dbco.txt out/matrix.mtx out/genes.txt out/barcodes.txt
