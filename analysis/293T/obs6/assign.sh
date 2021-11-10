4b76ce25d95af6cdc30d0cb4c9f0726ac4e74c49
sbooeshaghi <sbooeshaghi@gmail.com>
2021-11-09 17:28:12 -0800

    using mx filter and adding the mahalanobis distance to the assignments metadata

--- analysis/293T/obs6/assign.sh
+++ analysis/293T/obs6/assign.sh
@@ -3,5 +3,5 @@ gunzip out/*.gz
 ../../assign/mx_index.py assign/markers.txt assign/
 ../../assign/mx_select.py assign/markers.txt out/genes.txt assign/
 ../../assign/mx_extract.py out/matrix.mtx assign/select.txt assign/matrix_select.mtx
-../../assign/mx_assign.py assign/matrix_select.mtx assign/markers.ec assign/assignments.txt.gz out/barcodes.txt
+../../assign/mx_assign.py assign/matrix_select.mtx assign/markers.ec assign/assignments.txt.gz out/barcodes.txt assign/groups.txt
 gzip out/dbco.txt out/matrix.mtx out/genes.txt out/barcodes.txt
