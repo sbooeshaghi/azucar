7ba1ad5a2bc322e9afbb89ab593785b31f0619f2
sbooeshaghi <sbooeshaghi@gmail.com>
2022-02-03 10:54:07 -0800

    clean obs2

--- analysis/293T/obs2/assign.sh
+++ analysis/293T/obs2/assign.sh
@@ -4,4 +4,4 @@ gunzip out/*.gz
 ../../assign/mx_select.py assign/markers.txt out/genes.txt assign/
 ../../assign/mx_extract.py out/matrix.mtx assign/select.txt assign/matrix_select.mtx
 ../../assign/mx_assign.py assign/matrix_select.mtx assign/markers.ec assign/assignments.txt.gz out/barcodes.txt assign/groups.txt
-gzip out/dbco.txt out/matrix.mtx out/genes.txt out/barcodes.txt
+gzip out/dbco.txt out/matrix.mtx out/genes.txt out/barcodes.txt out/tags.txt
